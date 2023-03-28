08490b07ca624fbad39784bd6c46165f5256d7b2
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2023-03-28 14:45:11 +0100

    added better functionality less errors

--- deathSearcher.js
+++ deathSearcher.js
@@ -11,35 +11,19 @@ export class deathSearcher {
       return convertedDate;
     } else {
       console.log("Error - incorrect input");
+      alert("Choose a correct date");
     }
   };
+
   dateCheck = (date) => {
-    if (typeof date === "string" && date.length === 10 && date.includes("-")) {
-      let dateArray = date.split("-");
-      if (
-        dateArray[0].length === 4 &&
-        ~~dateArray[0] == dateArray[0] &&
-        dateArray[1].length === 2 &&
-        ~~dateArray[1] == dateArray[1] &&
-        dateArray[2].length === 2 &&
-        ~~dateArray[2] == dateArray[2]
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
+    return typeof date === "string" && date.length === 10 && date.includes("-");
   };
+
   async searchAPI(parameter) {
     const url = `https://en.wikipedia.org/w/api.php?action=parse&origin=*&format=json&page=${parameter}&prop=wikitext&formatversion=2`;
     const response = await fetch(url);
-
     const data = await response.json();
-
     const parsedData = await data.parse;
-
     return parsedData;
   }
 
@@ -55,40 +39,54 @@ export class deathSearcher {
     const filteredList = splitList.filter(this.isPerson);
     return filteredList;
   };
+
   pickRandom = (list) => {
     return Math.floor(Math.random() * list.length);
   };
+
   formatEntry = (data) => {
     let num = this.pickRandom(data);
-    console.log("num - " + num);
     let selection = data[num];
-    console.log(selection);
     let filteredName = this.filterName(selection);
     let name = this.nameCheck(filteredName);
-    let info = this.removeRef(this.createInfo(selection).trim());
+    let info = this.removeEndline(
+      this.removeRef(
+        this.removeSquareBrackets(this.createInfo(selection).trim())
+      )
+    );
     return [name, info];
   };
+
   nameCheck = (name) => {
     return name.includes("|") ? name.split("|")[0] : name;
   };
+
   createInfo = (data) => {
     return data.includes("]], ") ? data.split("]], ")[1] : "";
   };
+
   filterName = (data) => {
-    return data.split("[[")[2].split("]]")[0];
+    console.log(data);
+    return data.split("&ndash; [[")[1].split("]]")[0];
   };
+
   removeRef = (info) => {
     return info.includes("<ref>") ? info.split("<")[0] : info;
   };
+
+  removeSquareBrackets = (info) => {
+    return info.includes("[") ? info.replace(/[\[\]']+/g, "") : info;
+  };
+
+  removeEndline = (info) => {
+    return info.includes("===") ? info.split("===")[0] : info;
+  };
+
   async run(date) {
     let convertedDate = this.convertDate(date);
-    console.log(convertedDate + " this is the converted date");
     let result = await this.searchAPI(convertedDate);
     let formattedResult = await this.formatData(result);
-    console.log(result);
-    console.log(formattedResult);
     let entry = await this.formatEntry(formattedResult);
-    console.log(entry);
     return entry;
   }
 }
